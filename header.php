ff876a2d29dc80001d75fcbcc5374e87c1579d0f
tmatthews0020 <tmatthews0020@gmail.com>
2018-01-28 17:01:50 -0500

    override primary theme color

--- header.php
+++ header.php
@@ -12,6 +12,7 @@
         <?php if ( is_singular() && get_option( 'thread_comments' ) ) wp_enqueue_script( 'comment-reply' ); ?>
         <?php wp_head(); ?>
     </head>
+
     <div class="border-bottom">
       <nav class="container">
         <div class="row">

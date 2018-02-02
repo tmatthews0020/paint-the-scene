5db6b48ab53e0e5ca22e8581dbb0420c2afc4faa
tmatthews0020 <tmatthews0020@gmail.com>
2018-02-01 21:20:36 -0500

    use bootstrap navbar

--- header.php
+++ header.php
@@ -13,21 +13,24 @@
         <?php wp_head(); ?>
     </head>
 
-    <div class="border-bottom">
-      <nav class="container">
-        <div class="row">
-          <div class="col-3">
-            <?php
-              if ( function_exists( 'the_custom_logo' ) ) {
-                the_custom_logo();
-              }
-            ?>
-          </div>
-          <div class="col-9">
-            <?php
-              wp_nav_menu( array( 'theme_location' => 'header-menu' ) );
-             ?>
-          </div>
-        </div>
-      </nav>
-    </div>
+<nav class="navbar navbar-expand-lg navbar-light bg-light border-bottom">
+  <a class="navbar-brand" href="#">
+    <?php
+      if ( function_exists( 'the_custom_logo' ) ) {
+        the_custom_logo();
+      }
+    ?>
+  </a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="navbarNav">
+    <ul class="navbar-nav">
+     <?php wp_nav_menu( array(
+       'theme_location' => 'header-menu',
+       'menu-item' => 'nav-link',
+       'current-ment-item' => 'active'
+      )); ?>
+    </ul>
+  </div>
+</nav>

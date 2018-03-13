40cfb5335db6cd3698d087d7d19b94cbf69b4070
tmatthews0020 <tmatthews0020@gmail.com>
2018-03-12 20:19:48 -0400

    add home page

--- footer.php
+++ footer.php
@@ -1,8 +1,12 @@
 <nav class="navbar navbar-dark bg-dark footer mt-3">
-  <div class="container">
+  <div class="container text-white">
     <div class="row">
-      <div class="text-center">
-        <p>copyright 2018 </p>
+      <div class="col-md-6">
+        <div>
+          <span>copyright <?php echo date("Y") ?> </span>
+        </div>
+      </div>
+      <div class=" col col-md-6">
       </div>
     </div>
   </div>
